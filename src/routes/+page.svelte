b4475337111d6c3801c8c76dcf707ee54cf879b5
FatRat1213 <geny475384g@mail.ru>
2025-02-24 21:42:41 +0400

    replace commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,66 @@
-<h1>Hi</h1>
+<header class="upperDiv">
+    <section class="logo">
+    <h1>Logo</h1>
+    </section>
+    <section class="authorization">
+        <div><p>Авторизация</p></div>
+        <div><p>Рестрация</p></div>
+    </section>
+</header>
+<nav class="navbar">
+    <button class="mainButton">Главная</button>
+    <button class="mainButton">Начать обмен</button>
+    <button class="mainButton">Мои обмены</button>
+    <button class="mainButton">Задать вопрос</button>
+</nav>
+<section class="mainText"><p class="textOfMain">Обмен книгами (буккроссер) становится всё более популярен.<br> Это даёт шанс бумажным
+    книгам продлить свою жизнь, помогает владельцам книг делиться хорошими историями и
+    получать новые впечатления.<br>
+    Все буккроссеры любят свои книги и любят их читать. Они щедрые, новаторские, дружелюбные,
+    добросердечные, веселые и образованные люди.<br>
+    Наш сайт предлагает совершить не просто обмен, а добавить к этому увлекательному
+    процессу элемент сюрприза.<br> Подбор книг для обмена будет выполнен по пожеланиям участников,
+    но только при получении книги станет известно, какая именно книга будет радовать своего
+    владельца.<br> Интересно? Тогда начинайте обмен и приглашайте своих друзей поучаствовать! </p>
+</section>
+
+<style type="text/css">
+    #border{
+        border: 3px solid #fff;
+    }
+    .upperDiv{
+        display: flex;
+        justify-content: space-between;
+    }
+    .authorization{
+        display: flex;
+    }
+    .navbar{
+        display: flex;
+        justify-content: space-between;
+    }
+    .mainText{
+        display: flex;  
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 5%;
+    }
+    .textOfMain{
+        
+        display: flex;  
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-family: 'GraphikTT',Arial,sans-serif;
+    font-weight: 400;
+    font-size: 20px;
+    width: 50%;
+    }
+    .mainButton{
+        background-color:#fff ;
+        color: black;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+   </style>
