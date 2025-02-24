7c6e4e0b21ebd05810c2d95150b0ec32c7406b01
autriz <explayinformer8519@gmail.com>
2025-02-24 23:41:18 +0400

    Обновил тесты

--- e2e/demo.test.ts
+++ e2e/demo.test.ts
@@ -1,8 +1,14 @@
 import { expect, test } from '@playwright/test';
 
-test('home page has expected h1', async ({ page }) => {
+test('home page has expected header, main and footer', async ({ page }) => {
 	await page.goto('/');
-	const locator = page.locator('h1');
-	await expect(locator).toBeVisible();
-	await expect(locator).toContainText('Hi');
+
+	const header = page.locator('header');
+	await expect(header).toBeVisible();
+
+	const main = page.locator('main');
+	await expect(main).toBeVisible();
+
+	const footer = page.locator('footer');
+	await expect(footer).toBeAttached();
 });
