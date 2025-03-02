ac968dd441ad7b4c311d6881aed394bcd24a7ec6
FatRat1213 <geny475384g@mail.ru>
2025-03-02 23:05:55 +0400

    Сделан каркас основной страницы

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,13 +1,187 @@
+<div class="MainDiv">
 <section class="mt-14 flex flex-col items-center justify-center">
-	<p class="font-graphiktt flex w-[50%] flex-col items-center justify-center text-xl font-medium">
-		Обмен книгами (буккроссер) становится всё более популярен.<br /> Это даёт шанс бумажным
-		книгам продлить свою жизнь, помогает владельцам книг делиться хорошими историями и получать
-		новые впечатления.<br />
-		Все буккроссеры любят свои книги и любят их читать. Они щедрые, новаторские, дружелюбные, добросердечные,
-		веселые и образованные люди.<br />
-		Наш сайт предлагает совершить не просто обмен, а добавить к этому увлекательному процессу элемент
-		сюрприза.<br /> Подбор книг для обмена будет выполнен по пожеланиям участников, но только
-		при получении книги станет известно, какая именно книга будет радовать своего владельца.<br
-		/> Интересно? Тогда начинайте обмен и приглашайте своих друзей поучаствовать!
+	<p id="MainText" class="font-graphiktt flex w-[50%] flex-col items-center justify-center text-xl font-medium">
+		Дай вторую жизнь бумажным книгам
 	</p>
 </section>
+</div>
+<div class="SecondDiv">
+	<section class="mt-14 flex flex-col items-center justify-center">
+		<p id="BookText" class="font-graphiktt flex w-[50%] flex-col items-center justify-center text-xl font-medium">
+			буккроссинг - это свободный обмен бумажными книгами в любых удобных для вас местах
+		</p>
+	</section>
+	</div>
+	<div class="ThirdDiv">
+		<section class="mt-14 flex flex-col items-center justify-center">
+			<p id="briefly" class="font-graphiktt flex w-[50%] flex-col items-center justify-center text-xl font-medium">
+				Кратко
+			</p>
+			<p id="about" class="font-graphiktt flex w-[50%] flex-col items-center justify-center text-xl font-medium">
+				О буккроссинге
+			</p>
+		</section>
+		<p id="ThirdText" class="font-graphiktt flex w-[50%] flex-col items-left justify-start text-xl font-medium">
+			Обмен книгами (буккроссер) становится всё более популярен. Это даёт шанс бумажным
+книгам продлить свою жизнь, помогает владельцам книг делиться хорошими историями и
+получать новые впечатления.
+		</p>
+		</div>
+		<div class="FourDiv">
+			<section class="mt-14 flex flex-col items-center justify-center">
+				<p id="Community" class="font-graphiktt flex w-[50%] flex-col items-center justify-center text-xl font-medium">
+					Комьюнити
+				</p>
+			</section>
+			<p id="FourText" class="font-graphiktt flex w-[50%] flex-col items-left justify-start text-xl font-medium">
+				Обмен книгами (буккроссер) становится всё более популярен. Это даёт шанс бумажным
+	книгам продлить свою жизнь, помогает владельцам книг делиться хорошими историями и
+	получать новые впечатления.
+			</p>
+			</div>
+			<div class="FiveDiv">
+				<section class="mt-14 flex flex-col items-center justify-center">
+					<p id="Security" class="font-graphiktt flex w-[50%] flex-col items-center justify-center text-xl font-medium">
+						А это безопасно?
+					</p>
+				</section>
+				<p id="FiveText" class="font-graphiktt flex w-[50%] flex-col items-left justify-start text-xl font-medium">
+					Наш сайт предлагает совершить не просто обмен, а добавить к этому увлекательному
+процессу элемент сюрприза. Подбор книг для обмена будет выполнен по пожеланиям участников,
+но только при получении книги станет известно, какая именно книга будет радовать своего
+владельца
+				</p>
+				</div>
+				<div class="MainDiv">
+					<section class="mt-14 flex flex-col items-center justify-center">
+						<p id="intrested" class="font-graphiktt flex w-[50%] flex-col items-center justify-center text-xl font-medium">
+							Интересно?
+						</p>
+						<p id="LastText" class="font-graphiktt flex w-[50%] flex-col items-center justify-center text-xl font-medium">
+							Тогда начинайте обмен и приглашайте своих друзей поучаствовать! 
+						</p>
+						<button class="button"><a href="#" class="hyperlink">Начать обмен</a></button>
+					</section>
+					</div>
+
+
+<style type="text\css">
+	.MainDiv{
+		background-color: #212717;
+		width: 100%;
+		height: 800px;
+	}
+	.SecondDiv{
+		background-color: #EFEEDE;
+		width: 100%;
+		height: 800px;
+	}
+	.ThirdDiv{
+		background-color: #212717;
+		width: 100%;
+		height: fit-content;
+	}
+	.FourDiv{
+		background-color: #EFEEDE;
+		width: 100%;
+		height: fit-content;
+	}
+	.FiveDiv{
+		background-color: #fff3cd;
+		width: 100%;
+		height: fit-content;
+	}
+	#MainText{
+		font-family: var(--font-family);
+font-weight: 400;
+font-size: 96px;
+text-align: center;
+color: #fff;
+margin-top: 10%;
+	}
+	#BookText{
+		font-family: var(--font-family);
+	font-weight: 400;
+	font-size: 64px;
+	text-align: center;
+	color: #000;
+	margin-top: 10%;
+	}
+	#briefly{
+		font-family: var(--third-family);
+font-style: italic;
+font-weight: 400;
+font-size: 40px;
+text-align: center;
+color: #fff;
+	}
+	#about{
+		font-family: var(--second-family);
+font-style: italic;
+font-weight: 400;
+font-size: 64px;
+text-align: center;
+color: #fff;
+	}
+	#ThirdText{
+		font-family: var(--second-family);
+font-style: italic;
+font-weight: 400;
+font-size: 64px;
+color: #fff;
+	}
+	#Community{
+		font-family: var(--second-family);
+font-style: italic;
+font-weight: 400;
+font-size: 96px;
+text-align: center;
+color: #000;
+	}
+	#FourText{
+		font-family: var(--second-family);
+font-weight: 400;
+font-size: 64px;
+color: #000;
+	}
+	#Security{
+		font-family: var(--font-family);
+font-weight: 400;
+font-size: 96px;
+text-align: center;
+color: #000;
+	}
+	#FiveText{
+		font-family: var(--font-family);
+font-weight: 500;
+font-size: 64px;
+color: #000;
+	}
+	#intrested{
+		font-family: var(--font-family);
+font-weight: 500;
+font-size: 96px;
+color: #fff;
+	}
+	#LastText{
+		font-family: var(--font-family);
+font-weight: 500;
+font-size: 64px;
+text-align: center;
+color: #fff;
+	}
+	.button{
+		border-radius: 30px;
+		background: #cfd0ab;
+		width: 1266px;
+height: 115px;
+cursor: pointer;
+	}
+	.hyperlink{
+		font-family: var(--font-family);
+font-weight: 500;
+font-size: 64px;
+text-align: center;
+color: #000;
+	}
+</style>
