c2e88e05c233a6604977fb43cdc696c5e2e5d404
autriz <explayinformer8519@gmail.com>
2025-03-30 18:16:58 +0400

    Переместил пропы вверх из-за возможной перезаписи

--- src/lib/components/Input.svelte
+++ src/lib/components/Input.svelte
@@ -7,11 +7,11 @@
 </script>
 
 <input
+	{...props}
 	class="border-input placeholder:text-muted-foreground focus-visible:ring-ring
-	flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-base shadow-sm
-	transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium
-	file:text-foreground focus-visible:outline-none focus-visible:ring-1
+	file:text-foreground flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-base
+	shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm
+	file:font-medium focus-visible:ring-1 focus-visible:outline-none
 	disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
 	bind:value
-	{...props}
 />
