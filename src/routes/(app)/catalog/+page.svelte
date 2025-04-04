223d35317a52a9e77441d6b00587a096cac87cb7
FatRat1213 <geny475384g@mail.ru>
2025-04-04 13:24:05 +0400

    добавлен каталог

---
+++ src/routes/(app)/catalog/+page.svelte
@@ -0,0 +1,219 @@
+<h1 class="MainText">Каталог книг</h1>
+<h3 class="SideText">Найдите книги для обмена или добавьте свои</h3>
+<section class="SearchMain">
+<input class="input" placeholder="Поиск...">
+<button class="categoryButton">Все категории</button>
+<button class="filterButton"><img></button>
+</section>
+<section>
+    <aside class="MainBlockOfCard">
+        <div class="Card">
+            <img>
+            <aside class="bottomSide">
+                <div class="bottomText">
+                <h4 class="NameOfBook">Название книги</h4>
+                <p class="author">Автор книги</p>
+                <div>
+                    <img>
+                    <p class="city">Москва</p>
+                </div>
+                </div>
+            </aside>
+        </div>
+        <div class="Card">
+            <img>
+            <aside class="bottomSide">
+                <div class="bottomText">
+                <h4 class="NameOfBook">Название книги</h4>
+                <p class="author">Автор книги</p>
+                <div>
+                    <img>
+                    <p class="city">Москва</p>
+                </div>
+                </div>
+            </aside>
+        </div>
+        <div class="Card">
+            <img>
+            <aside class="bottomSide">
+                <div class="bottomText">
+                <h4 class="NameOfBook">Название книги</h4>
+                <p class="author">Автор книги</p>
+                <div>
+                    <img>
+                    <p class="city">Москва</p>
+                </div>
+                </div>
+            </aside>
+        </div>
+        <div class="Card">
+            <img>
+            <aside class="bottomSide">
+                <div class="bottomText">
+                <h4 class="NameOfBook">Название книги</h4>
+                <p class="author">Автор книги</p>
+                <div>
+                    <img>
+                    <p class="city">Москва</p>
+                </div>
+                </div>
+            </aside>
+        </div>
+    </aside>
+    <aside class="MainBlockOfCard">
+        <div class="Card">
+            <img>
+            <aside class="bottomSide">
+                <div class="bottomText">
+                <h4 class="NameOfBook">Название книги</h4>
+                <p class="author">Автор книги</p>
+                <div>
+                    <img>
+                    <p class="city">Москва</p>
+                </div>
+                </div>
+            </aside>
+        </div>
+        <div class="Card">
+            <img>
+            <aside class="bottomSide">
+                <div class="bottomText">
+                <h4 class="NameOfBook">Название книги</h4>
+                <p class="author">Автор книги</p>
+                <div>
+                    <img>
+                    <p class="city">Москва</p>
+                </div>
+                </div>
+            </aside>
+        </div>
+        <div class="Card">
+            <img>
+            <aside class="bottomSide">
+                <div class="bottomText">
+                <h4 class="NameOfBook">Название книги</h4>
+                <p class="author">Автор книги</p>
+                <div>
+                    <img>
+                    <p class="city">Москва</p>
+                </div>
+                </div>
+            </aside>
+        </div>
+        <div class="Card">
+            <img>
+            <aside class="bottomSide">
+                <div class="bottomText">
+                <h4 class="NameOfBook">Название книги</h4>
+                <p class="author">Автор книги</p>
+                <div>
+                    <img>
+                    <p class="city">Москва</p>
+                </div>
+                </div>
+            </aside>
+        </div>
+    </aside>
+</section>
+<section class="Buttons">
+    <button class="selectButton">1</button>
+    <button class="selectButton">2</button>
+    <button class="selectButton">3</button>
+</section>
+
+<style type="text/css">
+.bottomSide{
+    background: #212717;
+    width: 100%;
+    height: 30%;
+    margin-top: 100%;
+    
+}
+.MainText{
+    font-family: var(--font-family);
+font-weight: 700;
+font-size: 48px;
+color: #000;
+margin-left: 3%;
+}
+.SideText{
+    font-family: var(--font-family);
+font-weight: 400;
+font-size: 32px;
+color: #000;
+margin-left: 3%;
+}
+.MainBlockOfCard{
+    margin-top: 2%;
+    display: flex;
+}
+.NameOfBook{
+    font-family: var(--font-family);
+font-weight: 600;
+font-size: 24px;
+color: #e8e3d0;
+}
+.author{
+    font-family: var(--font-family);
+font-weight: 300;
+font-size: 20px;
+color: #e8e3d0;
+}
+.city{
+    font-family: var(--font-family);
+font-weight: 600;
+font-size: 16px;
+color: #e8e3d0;
+}
+.filterButton{
+    border: 1px solid #000;
+border-radius: 5px;
+width: 94px;
+height: 88px;
+background: #fff;
+}
+.Card{
+    border-radius: 28px;
+width: 406px;
+height: 644px;
+background: #d9d9d9;
+margin-left: 3%;
+}
+.bottomText{
+    margin-left: 7%;
+    
+}
+.input{
+    border: 1px solid #000;
+border-radius: 15px;
+width: 1266px;
+height: 88px;
+background: #fff;
+margin-left: 3%;
+}
+.categoryButton{
+    border: 1px solid #000;
+border-radius: 10px;
+width: 352px;
+height: 88px;
+background: #fff;
+}
+.Buttons{
+    display: flex;
+    justify-content: center;
+    margin-top: 2%;
+}
+.selectButton{
+    border: 1px solid #000;
+border-radius: 15px;
+width: 66px;
+height: 64px;
+background: #fff;
+font-family: var(--font-family);
+font-weight: 600;
+font-size: 32px;
+color: #000;
+cursor: pointer;
+margin-left: 1%;
+}
+</style>
