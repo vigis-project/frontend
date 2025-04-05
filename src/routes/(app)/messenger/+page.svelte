2fd24cc60acb26e5a5fb04e47416751464b74faf
FatRat1213 <geny475384g@mail.ru>
2025-04-05 19:28:18 +0400

    Добавлена страничка сообщений и чата

---
+++ src/routes/(app)/messenger/+page.svelte
@@ -0,0 +1,83 @@
+<div class="SideBlock">
+    <h1 class="MainText">Сообщения</h1>
+<section class="MainBlock">
+<input class="search" placeholder="Search...">
+<div>
+    <section class="message">
+        <img class="avatar">
+        <div class="divOfText"><p class="dialog">Леонид Яковлев</p><p class="dialog">Привет</p></div>
+    </section>
+    <section class="message">
+        <img class="avatar">
+        <div class="divOfText"><p class="dialog">Леонид Яковлев</p><p class="dialog">Привет</p></div>
+    </section>
+    <section class="message">
+        <img class="avatar">
+        <div class="divOfText"><p class="dialog">Леонид Яковлев</p><p class="dialog">Привет</p></div>
+    </section>
+    <section class="message">
+        <img class="avatar">
+        <div class="divOfText"><p class="dialog">Леонид Яковлев</p><p class="dialog">Привет</p></div>
+    </section>
+    <section class="message">
+        <img class="avatar">
+        <div class="divOfText"><p class="dialog">Леонид Яковлев</p><p class="dialog">Привет</p></div>
+    </section>
+</div>
+</section>
+</div>
+
+<style type="text/css">
+.SideBlock{
+    background: #212717;
+    border-radius: 15px;
+    width: 95%;
+    height: 100%;
+    margin-left: 2%;
+}
+.MainText{
+    font-family: var(--font-family);
+    font-weight: 400;
+    font-size: 48px;
+    color: #ffffff;
+    margin-left: 2%;
+}
+.MainBlock{
+    background: #cfd0ab;
+    border-radius: 10px;
+}
+.search{
+    border: 1px solid #000;
+border-radius: 15px;
+height: 79px;
+width: 80%;
+font-family: var(--font-family);
+font-weight: 400;
+font-size: 48px;
+color: #000;
+margin-left: 8%;
+}
+.message{
+    display: flex;
+}
+.avatar{
+    border-radius: 100%;
+    width: 90px;
+    height: 90px;
+    background-color: #d9d9d9;
+    margin-left: 2%;
+    margin-top: 2%;
+    cursor: pointer;
+}
+.dialog{
+    font-family: var(--font-family);
+font-weight: 400;
+font-size: 24px;
+color: #000;
+cursor: pointer;
+}
+.divOfText{
+    margin-left: 2%;
+    margin-top: 2%;
+}
+</style>
